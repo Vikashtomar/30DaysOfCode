eaf0029dfa4e086c2a3cb44b337453e5e8d04841
vikash <vikash.tomar.india@gmail.com>
2023-11-11 17:11:22 +0530

    day six

---
+++ DaySix.js
@@ -0,0 +1,22 @@
+/** @format */
+
+// **Challenge 6**
+// Write a JavaScript program to compute the sum of the two given integers.
+//  If the two values are the same, then return triple their sum.
+
+function sumOfnumber(a,b){
+
+    if(a == b){
+        return 3 * (a+b)
+    }
+    else {
+        return a+b
+    }
+
+}
+ let result = sumOfnumber(5,5)
+ console.log(result)
+
+
+
+
