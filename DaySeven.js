8bb62092c2c8495830a1afc23fff9bc37f19cb25
vikash <vikash.tomar.india@gmail.com>
2023-11-12 17:56:33 +0530

    day seven

---
+++ DaySeven.js
@@ -0,0 +1,17 @@
+ // **Challenge 7**
+// Write a JavaScript program to check a pair of numbers and return true
+//  if one of the numbers is 50 or if their sum is 50.
+
+function paircheckfor50(num, num2) {
+  if (num === 50) {
+    return true;
+  } else if (num2 === 50) {
+    return true;
+  } else if (num + num2 == 50) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(paircheckfor50(50));
