28a150c0fc1c010fed4da57e6395de4b87f48baf
Vikash Tomar <74128139+Vikashtomar@users.noreply.github.com>
2023-12-28 21:54:48 +0530

    Update DayOne.js

--- DayOne.js
+++ DayOne.js
@@ -10,7 +10,7 @@ function reverseNum(num){
     let reversed = 0;
     while(num > 0){
         reversed = (reversed * 10) + (num % 10);
-        num = Math.floor(num / 10);
+       num = Math.floor(num / 10);
          
     }
     return reversed;
