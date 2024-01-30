ca7020bedcf1dc7c4a27c297ccd38f54fd0c87f3
vikash <vikash.tomar.india@gmail.com>
2024-01-30 23:34:19 +0530

    Factorials

--- Day19.js
+++ Day19.js
@@ -6,9 +6,20 @@
 // .catch(error => console.error('Error',error))
 
 
-function getRandom(min,max){
-    return Math.floor(Math.random() * (max - min +1) + min);
+// function getRandom(min,max){
+//     return Math.floor(Math.random() * (max - min +1) + min);
 
-}
+// }
 
-console.log(getRandom(6,10))
+// console.log(getRandom(6,10))
+
+ function factorial(n){
+    if(n === 0 || n === 1){
+        return 1;
+    }
+    else{
+        return n * factorial(n-1);
+    }
+ }
+ const num = 7;
+ console.log(`Factorial of ${num}`,factorial(num));
